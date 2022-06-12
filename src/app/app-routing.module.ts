e026f9b81e46151719d87364f92481f8553547a9
ilyesbrh <brhilyes@gmail.com>
2022-06-12 19:34:45 +0100

    cosmetic changes
    - display products component in root URL

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { DisplayProductsComponent } from './display-products/display-products.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DisplayProductsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
